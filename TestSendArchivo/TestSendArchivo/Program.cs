76ce5ee820c086ca80046efd911af3c09b38c897
Juan Cruz <juancruzaraujo@gmail.com>
2021-03-25 10:02:15 -0300

    corrección mínima
    se corrigió el tipo de evento de SERVER_STAR a SERVER_START

--- TestSendArchivo/TestSendArchivo/Program.cs
+++ TestSendArchivo/TestSendArchivo/Program.cs
@@ -213,7 +213,7 @@ namespace TestSendArchivo
                     Console.WriteLine("<<conexión fin>> " + corchete(ev.GetConnectionNumber.ToString()) + " >>");
                     break;
 
-                case EventParameters.EventType.SERVER_STAR:
+                case EventParameters.EventType.SERVER_START:
                     Console.WriteLine("<<server iniciado>>");
                     break;
 
